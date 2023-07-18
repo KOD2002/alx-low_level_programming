8a7c86f9e3a7d04bef5156e1773a33ca37d40b00
KOD2002 <kwabosei000@gmail.com>
2023-07-18 04:11:33 -0700

    prints long sentence

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,7 +3,7 @@
 
 /**
  * main - Entry point
- * Description: 'prints and that piece of art is useful - Dora Korpar, 2015-10-19'
+ * Description: 'and that piece of art is useful - Dora Korpar, 2015-10-19'
  * Return: Always 0
  */
 int main(void)
